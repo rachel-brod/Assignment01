1b4130e6abc225ff6058fe4553a04a09e991e388
rachel-brod <124601086+rachel-brod@users.noreply.github.com>
2023-02-13 22:08:28 -0800

    completed assignment

---
+++ js/script.js
@@ -0,0 +1,159 @@
+// STEP 1
+/*
+let someMonth
+function theMonth()
+let currentMonth
+let summerMonth
+let myLibraryFunction 
+*/
+
+// STEP 2
+/*
+Numeric literal expression
+    15
+String literal expression
+    'Top of the morning to you.'
+Boolean literal expression
+    true
+Null literal expression 
+    null
+*/
+
+// STEP 3
+/*
+let myName = 'Rachel'
+let myAgeThisYear = 2023 - 1997
+*/
+
+// STEP 4
+/*
+let strFirstName
+let strLastName
+let strAddress
+let strCity
+let strState
+let strZipCode
+let intYourAge
+let strReferralSource
+let blnMayWeContactYou
+*/
+
+// STEP 5
+/*
+let strFirstName = 'Rachel'
+let intYourAge = 25
+let blnMayWeContactYou = false
+*/
+
+// STEP 6
+/*
+let myAge = 25
+console.log ('I am ' + myAge + ' years old')
+*/
+
+// STEP 7
+/*
+let inComp690 = true
+console.log ('It is ' + inComp690 + ' that I am in Comp 690')
+
+let x = 3
+console.log(x+inComp690)
+*/
+
+// STEP 8
+/*
+No, it should look like this:
+    let someString = 'Who once said, \"Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former.\"'
+    console.log(someString)
+*/
+
+// STEP 9
+/*
+let x = null
+let myVariable
+*/
+
+// STEP 10
+/*
+console.log(typeof 'my string')
+console.log(typeof 3)
+console.log(typeof false)
+console.log(typeof null)
+console.log(typeof undefinedVariable)
+*/
+
+// STEP 11
+/*
+alert('Hello ' + 'Rachel Brod' + ', welcome to the JavaScript class!')
+*/
+
+// STEP 12
+/*
+let name = 'Rachel Brod'
+alert('Hello ' + name + ', welcome to the JavaScript class!')
+*/
+
+// STEP 13
+/*
+let course = 'JavaScript'
+alert('Hello ' + name + ', welcome to the ' + course + ' class!')
+*/
+
+// STEP 14
+/*
+alert('Hello ' + name + '.\nWelcome to the ' + course + ' class!')
+*/
+
+// STEP 15
+/*
+let name = prompt('What is your name?')
+alert('Hello ' + name + '.\nWelcome to the ' + course + ' class!')
+*/
+
+// STEP 16
+/*
+let course = prompt('What class are your taking?')
+alert('Hello ' + name + '.\nWelcome to the ' + course + ' class!')
+*/
+
+// STEP 17
+/*
+let x = 10
+let y = 20
+console.log(x+y)
+*/
+
+// STEP 18
+/*
+let x = 20
+x +=20
+console.log(x)
+*/
+
+// STEP 19
+/*
+let x = 20
+x *= 5
+console.log(x)
+*/
+
+// STEP 20
+/*
+let x = 20 % 3
+x /= 1
+console.log(x)
+*/
+
+// STEP 21
+/*
+let x = 20
+let y = 'high five'
+console.log(x === 20 && y === 'high five')
+*/
+
+// STEP 22
+/*
+let x = 20
+let y = 'high five'
+console.log(x > 25 || y != 'high five')
+*/
